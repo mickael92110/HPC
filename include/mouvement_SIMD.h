7bfc12e47e0103e5729a0c502f45a4020c4e10fb
mickael92110 <mickael.ramilison@gmail.com>
2020-11-25 17:09:49 +0100

    ajout macro deroulement

--- include/mouvement_SIMD.h
+++ include/mouvement_SIMD.h
@@ -25,6 +25,28 @@
               kt = _mm_and_si128(c,k1);     \
               d = _mm_sub_epi8(d,kt)
 
+#define init_step1(v)                       \
+              t = ld(SigmaDelta_step0[k][i+BORD/2][j+((BORD/2)/CARD)+v]); \
+              st(SigmaDelta_step1[k][i+BORD/2][j+(BORD/(2*CARD))+v],_mm_add_epi8(t,k0))
+
+#define init_step1_switch(v)                       \
+              t = ld(SigmaDelta_step0[k][i+BORD/2][((BORD/2)/CARD)+l/CARD-r+v]); \
+              st(SigmaDelta_step1[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v],_mm_add_epi8(t,k0))
+
+#define ld_st_step1(v)                      \
+              d = ld(SigmaDelta_step1[k-1][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              a = d;  \
+              b = ld(SigmaDelta_step0[k][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              traitement_step1; \
+              st(SigmaDelta_step1[k][i+BORD/2][j+(BORD/(2*CARD))+v],d)
+
+#define ld_st_step1_switch(v)                      \
+              d = ld(SigmaDelta_step1[k-1][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              a = d;  \
+              b = ld(SigmaDelta_step0[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              traitement_step1; \
+              st(SigmaDelta_step1[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v],d)
+
 #define traitement_step2                    \
               n1 = _mm_cmplt_epi8 (b,k0);   \
               n2 = _mm_cmplt_epi8 (a,k0);   \
@@ -37,7 +59,82 @@
               dn = _mm_sub_epi8(c,dn);      \
               n1 = _mm_and_si128(c,k1);     \
               dn = _mm_add_epi8(dn,n1);     \
-              d = _mm_add_epi8(dn,kn)      
+              d = _mm_add_epi8(dn,kn)
+
+#define ld_st_step2(v)                      \
+              a = ld(SigmaDelta_step1[k][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              b = ld(SigmaDelta_step0[k][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              traitement_step2; \
+              st(SigmaDelta_step2[k][i+BORD/2][j+(BORD/(2*CARD))+v],d)
+
+#define ld_st_step2_switch(v)                      \
+              a = ld(SigmaDelta_step1[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              b = ld(SigmaDelta_step0[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              traitement_step2; \
+              st(SigmaDelta_step2[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v],d)
+
+#define traitement_step3                    \
+              n1 = _mm_cmplt_epi8 (b,k0);   \
+              n2 = _mm_cmplt_epi8 (a,k0);   \
+              c =  _mm_cmplt_epi8 (a,b);    \
+              c = _mm_xor_si128(c,n1);      \
+              c = _mm_xor_si128(c,n2);      \
+              kt = _mm_and_si128(c,k1);     \
+              d = _mm_add_epi8(d,kt);       \
+              c = _mm_cmpgt_epi8 (a,b);     \
+              c = _mm_xor_si128(c,n2);      \
+              c = _mm_xor_si128(c,n1);      \
+              kt = _mm_and_si128(c,k1);     \
+              d = _mm_sub_epi8(d,kt);       \
+              c = _mm_cmpeq_epi8 (d,vmax_v);\
+              n1= _mm_and_si128(c,k1);      \
+              d = _mm_sub_epi8(d,n1);       \
+              c = _mm_cmpeq_epi8 (d,vmin_v);\
+              n1= _mm_and_si128(c,k1);      \
+              d = _mm_add_epi8(d,n1)
+
+#define init_step3(v)                       \
+st(SigmaDelta_step3[k][i+BORD/2][j+(BORD/(2*CARD))+v],_mm_set1_epi8(vmin+1))
+
+
+#define init_step3_switch(v)                       \
+st(SigmaDelta_step3[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v],_mm_set1_epi8(vmin+1));
+
+#define ld_st_step3(v)                      \
+              d = ld(SigmaDelta_step3[k-1][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              a = d; \
+              b = ld(SigmaDelta_step2[k][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              b = _mm_add_epi8(b,b); \
+              traitement_step3; \
+              st(SigmaDelta_step3[k][i+BORD/2][j+(BORD/(2*CARD))+v],d)
+
+#define ld_st_step3_switch(v)                      \
+              d = ld(SigmaDelta_step3[k-1][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              a = d; \
+              b = ld(SigmaDelta_step2[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              b = _mm_add_epi8(b,b); \
+              traitement_step3; \
+              st(SigmaDelta_step3[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v],d)
+
+#define traitement_step4                    \
+              n1 = _mm_cmplt_epi8 (b,k0);   \
+              n2 = _mm_cmplt_epi8 (a,k0);   \
+              c = _mm_cmplt_epi8 (a,b);     \
+              c = _mm_xor_si128(c,n2);      \
+              c = _mm_xor_si128(c,n1);      \
+              d = _mm_and_si128(c,k255)
+
+#define ld_st_step4(v)                      \
+              a = ld(SigmaDelta_step2[k][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              b = ld(SigmaDelta_step3[k][i+BORD/2][j+(BORD/(2*CARD))+v]); \
+              traitement_step4; \
+              st(SigmaDelta_step4[k][i+BORD/2][j+(BORD/(2*CARD))+v],d)
+
+#define ld_st_step4_switch(v)                      \
+              a = ld(SigmaDelta_step2[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              b = ld(SigmaDelta_step3[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v]); \
+              traitement_step4; \
+              st(SigmaDelta_step4[k][i+BORD/2][(BORD/(2*CARD))+l/CARD-r+v],d); \
 
 vuint8*** init_tab_SIMD(int h, int l, int n);
 void init_bord(vuint8*** SigmaDelta_step4_SIMD, int h,int l, int n,int bord);
